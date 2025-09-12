f7555930633aed02ba9b7b176ed36b0cbb9f3a9b
Fátima Rodrigues <fatimarodrigues@Mac.localdomain>
2025-09-12 19:36:24 +0100

    fix github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,10 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  site: 'https://fatimaria7.github.io',
+  base: '/portfolio-main',
   integrations: [react(), tailwind()],
+  build: {
+    assets: '_astro'
+  }
 });
