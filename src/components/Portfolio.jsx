dec1ef479d8dff7f7a4bc1b2d178a893049fe782
Fátima Rodrigues <fatimarodrigues@Mac.localdomain>
2026-01-27 19:45:13 +0000

    new favicon

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,7 +7,7 @@ const Portfolio = () => {
       <Helmet>
           <meta charset="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <title>Portfolio | Fátima Rodrigues</title>
+          <title>Fátima Rodrigues | Portfolio</title>
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
           <link rel="icon" type="image/svg+xml" href="/portfolio/Favicon-light.svg" />
         </Helmet>
